e97a940692d17dc32c5ccb3448615def8f3b5a22
Syed Javaid <s.java458@gmail.com>
2018-09-07 15:29:45 -0400

    this is the real differnce

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,7 +9,7 @@ class Modal extends Component {
     shouldComponentUpdate ( nextProps, nextState ) {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
-    
+    //this is correct
     componentWillUpdate () {
         console.log('[Modal] WillUpdate');
     }
